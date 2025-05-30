fd4dc19869e0a1f5360e683847d0a535206b264c
hect95 <hector_cruz95@live.com>
2025-05-30 17:48:48 +0100

    Add data manager module to detect new ROS bags
    - Introduce `data_manager` module to detect and handle new ROS bags
      from a specified directory.
      - Use `watchdog` library and a custom `NewRosbagsWatchdog` handler
        to monitor the directory for new ROS bag recordings
    - Add `new_rosbag_watchdog` to define `NewRosbagsWatchdog` class
      behaviour and criteria for detecting new ROS bags.
    - Add `rosbag_metadata_parser` module to get the list of ROS bag files
      associated with a specific ROS bag recording directory.
    - Install watchdog and colorlog libraries in the Dockerfile
    - Add `data_manager` package to `pyproject.toml` file so
      it can also be installed.

---
+++ scripts/data_manager/__init__.py
@@ -0,0 +1,1 @@
+"""Initialise the data_manager package."""
---
+++ scripts/data_manager/rosbag_metadata_parser.py
@@ -0,0 +1,38 @@
+"""Parse metadata from ROS bag recordings."""
+
+from pathlib import Path
+
+import yaml
+
+
+class RosbagMetadataParser:
+    """
+    Parse the metadata file from a ROS bag recording.
+
+    This class reads a `metadata.yaml` file located in a ROS bag recording
+    directory and extracts the list of expected `.mcap` files.
+    """
+
+    def get_expected_rosbag_files(
+        self, metadata_file_path: Path
+    ) -> list[Path]:
+        """
+        Get expected ROS bag files from the recording metadata.
+
+        Args:
+            metadata_file_path: Path to the metadata file.
+
+        Returns:
+            List of expected ROS bag file paths.
+        """
+        with open(metadata_file_path) as file:
+            metadata = yaml.safe_load(file)
+            recording_info = metadata.get('rosbag2_bagfile_information')
+            rosbag_files_list = recording_info.get('relative_file_paths')
+            parent_directory = metadata_file_path.parent
+            rosbag_files = []
+            for rosbag_file in rosbag_files_list:
+                rosbag_file_path = parent_directory / rosbag_file
+                rosbag_files.append(rosbag_file_path)
+
+            return rosbag_files
